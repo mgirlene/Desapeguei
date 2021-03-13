8f2a5475c5bcd9cf3c516a536003bbadf4d4b915
mgirlene <mgirlene.silva0908@gmail.com>
2021-03-13 19:12:47 -0300

    update de usuário e conclusão do reset senha.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserCreate
+from .views import UserCreate,UserUpdate
 
 urlpatterns = [
     path('cadastro/', UserCreate.as_view(), name="userCreate"),
+    path('editar/<int:pk>/', UserUpdate.as_view(), name="userUpdate"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,39 @@
-from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView, UpdateView
 from .models import Usuario
 from .forms import UsuarioRegisterForm
 from django.urls import reverse_lazy
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
 
 class UserCreate(CreateView):
     model = Usuario
     template_name = 'accounts/register.html'
     form_class = UsuarioRegisterForm
-    success_url = reverse_lazy('login')
+    success_url = 'login'
+
+    def form_valid(self, form):
+        self.usuario = form.save()
+
+        send_mail(
+            'Cliente Cadastrado',
+            '%s, Você foi cadastrado com sucesso!' % self.usuario.first_name,
+            'desapegueidsc@gmail.com',
+            [self.usuario.email],
+            fail_silently=False,
+        )
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        messages.success(self.request, 'Usuário cadastrado com sucesso!')
+        return reverse_lazy(self.success_url)
+
+
+class UserUpdate(LoginRequiredMixin, UpdateView):
+    model = Usuario
+    fields = ('first_name', 'last_name', 'cpf', 'data_nascimento', 'celular')
+    template_name = 'accounts/update_user.html'
+    success_url = reverse_lazy('index')

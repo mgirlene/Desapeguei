3e6032c4473f8a9a61ebe3db87af189b81dfdb67
neudinho11 <neudinho66@gmail.com>
2021-03-19 12:35:04 -0300

    Ajustes produtos

--- gerenciador/views.py
+++ gerenciador/views.py
@@ -10,12 +10,9 @@ class Index(ListView):
     template_name = 'gerenciador/index.html'
 
     def get_queryset(self):
-        print("njbghjebhgbejghberjhgb")
         id_user = self.request.user.id
         if id_user:
             produtos = Produto.objects.filter(~Q(fk_usuario=id_user))
         else:
             produtos = Produto.objects.all()
-        print("88855555555665555")
-        print(produtos)
         return produtos
